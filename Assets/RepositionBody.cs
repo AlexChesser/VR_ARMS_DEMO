9be6b6c2265181226678d48f049927cc762e504c
Alexander Chesser <alex.chesser@gmail.com>
2020-01-12 17:18:12 -0500

    initial commit, arm tracking + IK

---
+++ Assets/RepositionBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepositionBody : MonoBehaviour
+{
+
+    [SerializeField] private Transform anchor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = anchor.position + (Vector3.down * 1.65f) + (Vector3.back * 0.1f);
+    }
+}
---
+++ Assets/VRIK.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRIK : MonoBehaviour
+{
+    private Animator animator;
+    private float IKWeight = 1.0f;
+    [SerializeField] private Transform IKTarget_Head;
+    [SerializeField] private Transform IKTarget_LH;
+    [SerializeField] private Transform IKTarget_RH;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        animator.SetLookAtWeight(IKWeight);
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, IKWeight);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, IKWeight);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, IKWeight);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightHand, IKWeight);
+
+        animator.SetLookAtPosition(IKTarget_Head.position);
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, IKTarget_LH.position);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, IKTarget_RH.position);
+        animator.SetIKRotation(AvatarIKGoal.LeftHand, IKTarget_LH.rotation);
+        animator.SetIKRotation(AvatarIKGoal.RightHand, IKTarget_RH.rotation);
+    }
+}
